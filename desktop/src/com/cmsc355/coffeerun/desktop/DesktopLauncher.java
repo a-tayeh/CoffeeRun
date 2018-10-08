3630494015f15c58af704c6b411c9adf4f817990
AliTayeh <tayehas@vcu.edu>
2018-10-08 19:40:45 -0400

    init commit

---
+++ desktop/src/com/cmsc355/coffeerun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cmsc355.coffeerun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cmsc355.coffeerun.CoffeeRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoffeeRun(), config);
+	}
+}
